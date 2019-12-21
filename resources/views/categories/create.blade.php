8bd012e1ced25ff68d0885132f83c8fcfc224d9d
nutkiattisak <kiattisak.ch1995@gmail.com>
2019-12-21 23:38:25 +0700

    create categories form

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card card-default">
+    <div class="card-header">Categories</div>
+    <div class="card-body">
+        <form action="">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <button class="btn btn-success">Add Category</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
