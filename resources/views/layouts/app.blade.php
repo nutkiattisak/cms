2d57c42c0b2799d4b3228178c89f3461063ea75f
nutkiattisak <kiattisak.ch1995@gmail.com>
2019-12-22 21:58:54 +0700

    creating posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -29,13 +26,11 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -84,7 +79,7 @@
                     <div class="col-md-4">
                         <ul class="list-group">
                             <li class="list-group-item">
-                                <a href="">Posts</a>
+                                <a href="{{route('posts.index')}}">Posts</a>
                             </li>
                             <li class="list-group-item">
                             <a href="{{route('categories.index')}}">Categories</a>
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card card-default">
+        <div class="card-header">
+            Create Post
+        </div>
+        <div class="card-body">
+            <form action="{{ route('posts.store') }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea name="description" id="description" class="form-control" cols="5" rows="5"></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" id="content" class="form-control" cols="5" rows="5"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="published_at">Published At</label>
+                    <input type="text" name="published_at" id="published_at" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="image">Image</label>
+                    <input type="file" name="image" id="image" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success">Create Post</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
