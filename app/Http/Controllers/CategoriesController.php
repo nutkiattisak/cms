b461c7a49a4806c7371bbe0d9c42d23e4ca93cef
nutkiattisak <kiattisak.ch1995@gmail.com>
2019-12-22 11:19:58 +0700

    Create Request Category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Category;
 
 use Illuminate\Http\Request;
-
+use App\Http\Requests\CreateCategoryRequest;
 class CategoriesController extends Controller
 {
     /**
@@ -35,11 +35,11 @@ class CategoriesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateCategoryRequest $request)
     {
-        $this->validate($request, [
+        /*$this->validate($request, [
             'name' => 'required|unique:categories'
-        ]);
+        ]);*/
 
         Category::create([
             'name' => $request->name
